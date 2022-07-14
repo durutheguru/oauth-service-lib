b1ebc5a5ab1e90c198ac1b1ff2e31db4ec322797
Julian Duru <durutheguru@gmail.com>
2022-07-14 06:24:59 +0100

    - Polished workflow yml - Tweaked TestContainersConfig

--- src/test/java/com/julianduru/oauthservicelib/OAuthServiceLibIntegrationTest.java
+++ src/test/java/com/julianduru/oauthservicelib/OAuthServiceLibIntegrationTest.java
@@ -2,17 +2,11 @@ package com.julianduru.oauthservicelib;
 
 import com.github.javafaker.Faker;
 import com.julianduru.oauthservicelib.config.TestContainersConfig;
-import com.mysql.cj.jdbc.MysqlDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.ContainerState;
-import org.testcontainers.containers.DockerComposeContainer;
-
-import javax.sql.DataSource;
 
 /**
  * created by julian on 26/04/2022
@@ -32,8 +26,6 @@ public abstract class OAuthServiceLibIntegrationTest {
     protected Faker faker = new Faker();
 
 
-
 }
-
 
 
