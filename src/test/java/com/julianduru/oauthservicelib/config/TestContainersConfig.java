ac25c5c9a76b7d626d1dfef861563ddbd05653c2
Julian Duru <durutheguru@gmail.com>
2022-07-20 07:58:15 +0100

    - Corrected strategy for getting OAuth Service URL in test

--- src/test/java/com/julianduru/oauthservicelib/config/TestContainersConfig.java
+++ src/test/java/com/julianduru/oauthservicelib/config/TestContainersConfig.java
@@ -1,5 +1,6 @@
 package com.julianduru.oauthservicelib.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import java.time.Duration;
 /**
  * created by julian on 09/05/2022
  */
+@Slf4j
 @TestConfiguration
 public class TestContainersConfig {
 
@@ -40,25 +42,36 @@ public class TestContainersConfig {
     @ConditionalOnProperty(name = "testcontainers.enabled", havingValue = "true")
     public WebClient oauthServerGQLWebClient(
         DockerComposeContainer dockerComposeContainer
-//        , WebClientOAuthConfigurer webClientOAuthConfigurer
+        , WebClientOAuthConfigurer webClientOAuthConfigurer
     ) {
-        var containerStateOptional = dockerComposeContainer.getContainerByServiceName("oauth-service_1");
-        if (containerStateOptional.isPresent()) {
-            var containerState = (ContainerState) containerStateOptional.get();
-            var oauthServiceIp = containerState.getHost();
-            var portBinding = containerState.getPortBindings().get(0);
-            var oauthServicePort = Integer.parseInt(portBinding.substring(0, portBinding.indexOf(":")));
+//        var containerStateOptional = dockerComposeContainer.getContainerByServiceName("oauth-service_1");
+//        if (containerStateOptional.isPresent()) {
+//            var containerState = (ContainerState) containerStateOptional.get();
+//            var oauthServiceIp = containerState.getHost();
+//            var portBinding = containerState.getPortBindings().get(0);
+//            var oauthServicePort = Integer.parseInt(portBinding.substring(0, portBinding.indexOf(":")));
+//
+//            var baseUrl = String.format("http://%s:%d/graphql", oauthServiceIp, oauthServicePort);
+////            webClientOAuthConfigurer.configureWebClient(baseUrl);
+//            return WebClient.builder()
+////                .exchangeStrategies(exchangeStrategies)
+//                .baseUrl(baseUrl)
+////                .filter(oauth)
+//                .build();
+//        }
+//
+//        throw new IllegalStateException("Cannot find docker compose service name: oauth-service");
 
-            var baseUrl = String.format("http://%s:%d/graphql", oauthServiceIp, oauthServicePort);
-//            webClientOAuthConfigurer.configureWebClient(baseUrl);
-            return WebClient.builder()
-//                .exchangeStrategies(exchangeStrategies)
-                .baseUrl(baseUrl)
-//                .filter(oauth)
-                .build();
-        }
+        String oauthServiceUrl =
+            String.format(
+                "%s:%s/graphql",
+                dockerComposeContainer.getServiceHost("oauth-service_1", 10101),
+                dockerComposeContainer.getServicePort("oauth-service_1", 10101)
+            );
 
-        throw new IllegalStateException("Cannot find docker compose service name: oauth-service");
+        log.info("OAuth Service URL: {}", oauthServiceUrl);
+        
+        return webClientOAuthConfigurer.configureWebClient(oauthServiceUrl);
     }
 
 
