8d735602b05b6d4a0ec03a7d8e16128371b73881
Julian Duru <durutheguru@gmail.com>
2022-10-06 23:11:57 +0100

    - Added Auth Ping Controller

---
+++ src/main/java/com/julianduru/oauthservicelib/modules/authflow/AuthPingController.java
@@ -0,0 +1,20 @@
+package com.julianduru.oauthservicelib.modules.authflow;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by julian
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class AuthPingController {
+
+
+    @GetMapping
+    public void ping() { }
+
+
+}
