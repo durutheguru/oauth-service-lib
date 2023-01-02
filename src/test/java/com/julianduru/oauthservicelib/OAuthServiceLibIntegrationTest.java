29d66ebc292851ad37fdfe2dbdfc52eeb8473f68
Julian Duru <durutheguru@gmail.com>
2023-01-02 19:39:48 +0100

    - Stop docker compose on test completion

--- src/test/java/com/julianduru/oauthservicelib/OAuthServiceLibIntegrationTest.java
+++ src/test/java/com/julianduru/oauthservicelib/OAuthServiceLibIntegrationTest.java
@@ -4,12 +4,17 @@ import com.github.javafaker.Faker;
 import com.julianduru.oauthservicelib.config.TestContainersConfig;
 import com.julianduru.oauthservicelib.config.TestDatabaseConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.DockerComposeContainer;
 
 /**
  * created by julian on 26/04/2022
@@ -25,12 +30,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith({SpringExtension.class})
 @EnableConfigurationProperties({DataSourceProperties.class})
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class OAuthServiceLibIntegrationTest {
 
 
     protected Faker faker = new Faker();
 
 
-}
+    @Autowired
+    private DockerComposeContainer dockerComposeContainer;
 
+
+
+    @BeforeAll
+    public void beforeAll() {
+        log.info("Starting Docker Compose...");
+    }
+
+
+    @AfterAll
+    public void afterAll() {
+        log.info("Stopping Docker Compose...");
+        dockerComposeContainer.stop();
+    }
+
+
+}
 
